40252da96831de2a55324919a4143292e090918a
Ritesh Maharjan <ritesh@noveltytechnology.com>
2022-05-26 10:51:38 -0500

    fixed minor issue

---
+++ test.js
@@ -0,0 +1,20 @@
+function tets() {
+    var x = 10;
+    if (x == 10) {
+        let y = 5;
+        // console.log(y);
+    }
+    // let a = y;
+    // console.log(a);
+}
+
+// console.log(x);
+
+tets();
+
+var obj = {
+    x:3,
+    x:5
+}
+
+console.log(obj);
