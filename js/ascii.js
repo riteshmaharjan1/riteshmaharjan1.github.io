bef8d089063f34c9c37e3475808570fe404d1712
Ritesh Maharjan <ritesh@noveltytechnology.com>
2022-05-28 10:26:22 -0500

    formatted the code and added js

---
+++ js/ascii.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    "use strict";
+    // put all of your code here
+
+    let startButton = document.getElementById("start");
+    // startButton.onclick
+    console.log(startButton);
+}
